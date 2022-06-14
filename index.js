f6439a0253337de9f128d8851bc3c7651adbd9c2
Simeon <olabs06@yahoo.com>
2022-06-14 11:08:59 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards (names) {
+    const msg = [];
+    for (let i = 0; i < names.length; i++) {
+        msg[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`); 
+        
+    }
+
+    return msg;
+}
+
+writeCards(names);
+
+
+
+function countDown (param) {
+    let i = 0;
+    let b = param;
+
+    while (i <= param) {
+        console.log(b);
+        b--;
+        i++; 
+      }
+    
+      return param;
+    }
+    
+    countDown(param);
+
+
+
+
